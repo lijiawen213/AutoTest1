518b8bd054c24ca4ba7b0f578bdd6d83de048e6b
Rye0927 <36804931+Rye0927@users.noreply.github.com>
2019-07-25 15:04:28 +0800

    1111

---
+++ Chapter2/src/main/java/demo/demoTest.java
@@ -0,0 +1,24 @@
+package demo;
+
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demoTest {
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,2);
+    }
+
+    @Test
+    public void test3(){
+        Assert.assertEquals("aaa","aaa");
+    }
+
+    @Test
+    public void logDemo(){
+        Reporter.log("这是我们自己写的日志");
+        throw new RuntimeException("这是我自己的运行时异常");
+    }
+}
